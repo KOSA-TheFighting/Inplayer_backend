d6029a54eafd819dc8d4456b4df38fe64ebf2ce4
Seungbeom Kim <jhleeyoo2@naver.com>
2024-11-22 14:09:11 +0900

    관리자 기능 추가

--- src/main/java/com/webrtc/member/repository/AdminRepository.java
+++ src/main/java/com/webrtc/member/repository/AdminRepository.java
@@ -1,4 +1,23 @@
 package com.webrtc.member.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.ibatis.annotations.Mapper;
+import com.webrtc.member.model.dto.MemberDTO;
+
+
+@Mapper
 public interface AdminRepository {
+
+    // 회원 탈퇴 처리 (member_delete_yn을 1로 업데이트)
+    void deactivateMember(String member_id);
+
+    // 모든 회원 정보 조회
+    List<MemberDTO> findAllMembers();
+
+    // 특정 회원 정보 조회
+    Optional<MemberDTO> findMemberById(String member_id);
+
+
 }
