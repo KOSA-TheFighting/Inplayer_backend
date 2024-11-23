055ba793aae7c8d1b6f61b628d404604eb0cb1c7
docodack <docodack@naver.com>
2024-11-23 16:32:00 +0900

    Fix: 버그수정

--- src/main/java/com/webrtc/member/controller/AdminController.java
+++ src/main/java/com/webrtc/member/controller/AdminController.java
@@ -18,29 +18,29 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/admin")
 public class AdminController {
 
-    private final AdminService adminService;
+	private final AdminService adminService;
 
-    // 회원 탈퇴 처리
-    @PutMapping("/deactivate/{memberId}")
-    public ResponseEntity<Void> deactivateMember(@PathVariable String memberId) {
-        log.info("회원 탈퇴 요청: {}", memberId);
-        adminService.deactivateMember(memberId);
-        return ResponseEntity.ok().build();
-    }
+	// 회원 탈퇴 처리
+	@PutMapping("/deactivate/{memberId}")
+	public ResponseEntity<Void> deactivateMember(@PathVariable String memberId) {
+		log.info("회원 탈퇴 요청: {}", memberId);
+		adminService.deactivateMember(memberId);
+		return ResponseEntity.ok().build();
+	}
 
-    // 모든 회원 정보 조회
-    @GetMapping("members")
-    public ResponseEntity<List<MemberDTO>> getAllMembers() {
-        List<MemberDTO> members = adminService.getAllMembers();
-        System.out.println("전체 회원 목록: " + members);
-        return ResponseEntity.ok(members);
+	// 모든 회원 정보 조회
+	@GetMapping("members")
+	public ResponseEntity<List<MemberDTO>> getAllMembers() {
+		List<MemberDTO> members = adminService.getAllMembers();
+		System.out.println("전체 회원 목록: " + members);
+		return ResponseEntity.ok(members);
+	}
 
-
-    // 특정 회원 정보 조회
-    @GetMapping("/member/{member_id}")
-    public ResponseEntity<MemberDTO> getMemberById(@PathVariable String member_id) {
-        Optional<MemberDTO> member = adminService.getMemberById(member_id);
-        return member.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
+	// 특정 회원 정보 조회
+	@GetMapping("/member/{member_id}")
+	public ResponseEntity<MemberDTO> getMemberById(@PathVariable String member_id) {
+		Optional<MemberDTO> member = adminService.getMemberById(member_id);
+		return member.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	}
 
 }
